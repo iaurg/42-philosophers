d80c7dcaf156c0c6a2bf0ab5cd6a4beaef1bb0d9
Italo A <italoaurelior@gmail.com>
2022-07-20 23:06:20 -0300

    refactor: thread structure

--- philo/sources/threads.c
+++ philo/sources/threads.c
@@ -6,22 +6,21 @@
 /*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/12 22:03:24 by itaureli          #+#    #+#             */
-/*   Updated: 2022/07/19 22:58:43 by itaureli         ###   ########.fr       */
+/*   Updated: 2022/07/20 23:02:32 by itaureli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-static int	died(t_philo *philo)
+static void	died(t_philo *philo)
 {
 	t_table			*table;
 
 	table = philo->table;
-	print_message(philo, "died");
 	table->philo_alive = FALSE;
 	philo->is_alive = FALSE;
-	return (TRUE);
-
+	print_message(philo, "died");
+	return ;
 }
 
 static void philo_eat(t_philo *philo)
@@ -36,8 +35,8 @@ static void philo_eat(t_philo *philo)
 	print_message(philo, "has taken a fork");
 	print_message(philo, "has taken a fork");
 	print_message(philo, "is eating");
-	philo->count_meals = philo->count_meals + 1;
-	usleep(table->time_to_eat * 1000);
+	philo->count_meals++;
+	ft_usleep(table->time_to_eat);
 	pthread_mutex_unlock(philo->fork_left);
 	pthread_mutex_unlock(philo->fork_right);
 
@@ -54,7 +53,7 @@ static void philo_sleep(t_philo *philo)
 
 	table = philo->table;
 	print_message(philo, "is sleeping");
-	usleep(table->time_to_sleep * 1000);
+	ft_usleep(table->time_to_sleep);
 }
 
 void kill_the_one(t_philo *philo)
@@ -79,17 +78,14 @@ void	*start_dinner(void *arg)
 	if (table->number_of_philos == 1)
 		kill_the_one(philo);
 	if (philo->id % 2)
-		usleep(1500);
-	while (table->philo_alive != 0 && philo->is_alive != 0)
+		ft_usleep(table->time_to_eat / 10);
+	while (table->times_must_eat != philo->count_meals && (table->philo_alive != 0 && philo->is_alive != 0))
 	{
 		philo_eat(philo);
-		if(table->times_must_eat == philo->count_meals)
-			return NULL;
-		printf("time: %ld timetodie: %d\n", (actual_time() - philo->ts_last_meal), table->time_to_die);
-		if ((actual_time() - philo->ts_last_meal) >= (long)(table->time_to_die))
+		if ((actual_time() - philo->ts_last_meal) > table->time_to_die)
 		{
-			usleep(table->time_to_die * 1000);
 			died(philo);
+			ft_usleep(table->time_to_die);
 			return (NULL);
 		}
 		philo_sleep(philo);
@@ -102,21 +98,11 @@ int	init_threads(t_table *table)
 {
 	int i;
 
-	i = 0;
-	while(i < table->number_of_philos)
-	{
-		if(pthread_create(&table->philos[i]->thread, NULL, start_dinner, table->philos[i]))
-		{
-			printf("Error creating thread\n");
-			return (ERROR);
-		}
-		i++;
-	}
-	i = 0;
-	while(i < table->number_of_philos)
-	{
+	i = -1;
+	while(++i < table->number_of_philos)
+		pthread_create(&table->philos[i]->thread, NULL, start_dinner, table->philos[i]);
+	i = -1;
+	while(++i < table->number_of_philos)
 		pthread_join(table->philos[i]->thread, NULL);
-		i++;
-	}
 	return (SUCCESS);
 }
