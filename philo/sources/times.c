278314036a8fb98aa5d1e7b6482ede9d34e4e025
Italo A <italoaurelior@gmail.com>
2022-07-20 23:03:29 -0300

    feat: abstract func

--- philo/sources/times.c
+++ philo/sources/times.c
@@ -6,7 +6,7 @@
 /*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/17 20:47:42 by itaureli          #+#    #+#             */
-/*   Updated: 2022/07/19 22:57:21 by itaureli         ###   ########.fr       */
+/*   Updated: 2022/07/20 22:54:45 by itaureli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,4 +21,9 @@ long int	actual_time(void)
 	gettimeofday(&current_time, NULL);
 	time = (current_time.tv_sec * 1000) + (current_time.tv_usec / 1000);
 	return (time);
+}
+
+void	ft_usleep(int ms)
+{
+	usleep(ms * 1000);
 }
