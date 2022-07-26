8ee539e6bef88782f02a7f6ece5a35ad6b29417f
Italo A <italoaurelior@gmail.com>
2022-07-25 22:44:16 -0300

    fix: double print when died

--- philo/sources/actions.c
+++ philo/sources/actions.c
@@ -6,7 +6,7 @@
 /*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/12 21:18:46 by itaureli          #+#    #+#             */
-/*   Updated: 2022/07/23 15:37:01 by itaureli         ###   ########.fr       */
+/*   Updated: 2022/07/25 22:42:32 by itaureli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,12 @@
 void	print_message(t_philo *philo, char *msg)
 {
 	t_table			*table;
+	long			print_time;
 
 	table = philo->table;
+	print_time = actual_time() - table->ts_start;
 	pthread_mutex_lock(&table->message);
-	printf("%5ld %d %s\n", actual_time() - table->ts_start, philo->id + 1, msg);
+	if (table->philo_alive != 0)
+		printf("%5ld %d %s\n", print_time, philo->id + 1, msg);
 	pthread_mutex_unlock(&table->message);
 }
